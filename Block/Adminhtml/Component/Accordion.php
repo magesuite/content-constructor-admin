7e2472dce0cdee98e4a13a181e5f4c49abd43908
Blazej Doleska <blazej.doleska@creativestyle.pl>
2019-11-22 10:27:11 +0100

    added wysiwyg confiruration

--- Block/Adminhtml/Component/Accordion.php
+++ Block/Adminhtml/Component/Accordion.php
@@ -5,4 +5,24 @@ namespace MageSuite\ContentConstructorAdmin\Block\Adminhtml\Component;
 class Accordion extends \Magento\Framework\View\Element\Template
 {
     protected $_template = 'components/configurators/accordion.phtml';
+
+    /**
+     * @var \MageSuite\ContentConstructorAdmin\DataProviders\WysiwygConfigDataProvider
+     */
+    protected $wysiwygConfigDataProvider;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \MageSuite\ContentConstructorAdmin\DataProviders\WysiwygConfigDataProvider $wysiwygConfigDataProvider,
+        array $data = []
+    )
+    {
+        \Magento\Framework\View\Element\Template::__construct($context, $data);
+
+        $this->wysiwygConfigDataProvider = $wysiwygConfigDataProvider;
+    }
+
+    public function getWysiwygConfig() {
+        return $this->wysiwygConfigDataProvider->getConfig();
+    }
 }
