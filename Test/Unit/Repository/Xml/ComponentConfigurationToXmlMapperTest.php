e1ccedf46f68e441b07ba694c72a5567769fb42c
Krzysztof Kurzydło <krzysztof.kurzydlo@creativestyle.pl>
2020-08-24 16:02:20 +0200

    Compatibility with PHPUNIT 9

--- Test/Unit/Repository/Xml/ComponentConfigurationToXmlMapperTest.php
+++ Test/Unit/Repository/Xml/ComponentConfigurationToXmlMapperTest.php
@@ -125,8 +125,14 @@ class ComponentConfigurationToXmlMapperTest extends \PHPUnit\Framework\TestCase
      */
     public function testXmlRootNodeDoesNotExists($forbiddenString)
     {
-        $this->assertStringNotContainsString($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration, $this->existingXml)));
-        $this->assertStringNotContainsString($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration)));
+        if(method_exists($this, 'assertStringContainsString')) {
+            $this->assertStringNotContainsString($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration, $this->existingXml)));
+            $this->assertStringNotContainsString($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration)));
+        }
+        else {
+            $this->assertNotContains($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration, $this->existingXml)));
+            $this->assertNotContains($forbiddenString, strtolower($this->mapper->map($this->componentsConfiguration)));
+        }
     }
 
     /**
