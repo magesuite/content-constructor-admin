5d2892a87f37071c2efdc0ac2b3e745e5fceb16d
Paweł Złamaniec <pawel.zlamaniec@creativestyle.pl>
2021-02-24 09:13:41 +0100

    [GNP-564] Changed the way admin path is loaded

--- Helper/Configuration.php
+++ Helper/Configuration.php
@@ -6,14 +6,35 @@ class Configuration
 {
     const SECRET_TOKEN_ADMIN_CONTENT_CONSTRUCTOR_PREVIEW_PATH = 'magesuite/content_constructor_admin/preview_secret';
 
+    const CUSTOM_ADMIN_PATH = 'admin/url/custom_path';
+
     /**
      * @var \Magento\Framework\App\Config\ScopeConfigInterface
      */
     protected $scopeConfig;
 
-    public function __construct(\Magento\Framework\App\Config\ScopeConfigInterface $scopeInterface)
-    {
+    /**
+     * @var \Magento\Framework\App\DeploymentConfig\Reader
+     */
+    protected $configReader;
+
+    public function __construct(
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeInterface,
+        \Magento\Framework\App\DeploymentConfig\Reader $configReader
+    ) {
         $this->scopeConfig = $scopeInterface;
+        $this->configReader = $configReader;
+    }
+
+    public function getAdminPrefix()
+    {
+        $adminPath = $this->scopeConfig->getValue(self::CUSTOM_ADMIN_PATH);
+        if (!$adminPath) {
+            $config = $this->configReader->load();
+            $adminPath = $config['backend']['frontName'];
+        }
+
+        return $adminPath;
     }
 
     public function getSecretPreviewToken()
