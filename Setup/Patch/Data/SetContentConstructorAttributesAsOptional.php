e21399f1fb5c0216df8107e3c4477024bee1c124
Dariusz Matoga <dariusz.matoga@creativestyle.pl>
2021-08-19 07:57:30 +0000

    fix: Set CC attributes as optional

---
+++ Setup/Patch/Data/SetContentConstructorAttributesAsOptional.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace MageSuite\ContentConstructorAdmin\Setup\Patch\Data;
+
+class SetContentConstructorAttributesAsOptional implements \Magento\Framework\Setup\Patch\DataPatchInterface
+{
+    /**
+     * @var \Magento\Eav\Setup\EavSetup
+     */
+    protected $eavSetup;
+
+    protected $contentConstructorAttributesEntityTypes = [
+        \Magento\Catalog\Model\Category::ENTITY,
+        \Magento\Catalog\Model\Product::ENTITY
+    ];
+
+    protected $contentConstructorAttributes = [
+        \MageSuite\ContentConstructorAdmin\Setup\UpgradeData::CONTENT_CONSTRUCTOR_CONTENT_ATTRIBUTE_NAME,
+        \MageSuite\ContentConstructorAdmin\Setup\UpgradeData::LAYOUT_UPDATE_XML_BACKUP_ATTRIBUTE_NAME
+    ];
+
+    public function __construct(
+        \Magento\Eav\Setup\EavSetupFactory $eavSetupFactory,
+        \Magento\Framework\Setup\ModuleDataSetupInterface $moduleDataSetup
+    )
+    {
+        $this->eavSetup = $eavSetupFactory->create(['setup' => $moduleDataSetup]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        foreach ($this->contentConstructorAttributesEntityTypes as $contentConstructorAttributesEntityType) {
+            foreach ($this->contentConstructorAttributes as $contentConstructorAttribute) {
+                if (!$this->eavSetup->getAttribute($contentConstructorAttributesEntityType, $contentConstructorAttribute)) {
+                    continue;
+                }
+                $this->eavSetup->updateAttribute($contentConstructorAttributesEntityType, $contentConstructorAttribute, 'is_required', 'false');
+            }
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
