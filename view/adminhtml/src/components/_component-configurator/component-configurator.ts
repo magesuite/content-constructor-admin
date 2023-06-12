fdc149131e6c02c52429fa957f9d99db16e7351f
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2023-06-12 13:56:32 +0200

    feat: [KRA-36] Allow to set custom sections for etaser and text component

--- view/adminhtml/src/components/_component-configurator/component-configurator.ts
+++ view/adminhtml/src/components/_component-configurator/component-configurator.ts
@@ -80,35 +80,39 @@ const componentConfigurator: vuejs.ComponentOption = {
             return cssClassFields;
         },
         _collectComponentCssClasses(): void {
-            if (
-                this.ccConfig[this.xmlConfigEntry] != null &&
-                this.ccConfig[this.xmlConfigEntry].custom_sections != null
-            ) {
-                const cssClassFields: any[] = this._getCustomCssFields(this.ccConfig[this.xmlConfigEntry].custom_sections);
-                const cssClasses: string[] = [];
+            const componentsToIterate = this.secondaryXmlConfigEntry ? [this.xmlConfigEntry, this.secondaryXmlConfigEntry] : [this.xmlConfigEntry];
+            let cssClasses: string[] = [];
 
-                cssClassFields.forEach(
-                    (model: string) => {
-                        const configValue = this.configuration[model];
-
-                        if (!configValue) {
-                            return;
-                        }
-
-                        if (typeof configValue === 'string') {
-                            cssClasses.push(configValue);
-                        } else if (typeof configValue === 'object') {
-                            for (const key in configValue) {
-                                if (configValue.hasOwnProperty(key)) {
-                                    cssClasses.push(configValue[key]);
+            componentsToIterate.forEach((entry: string) => {
+                if (
+                    this.ccConfig[entry] != null &&
+                    this.ccConfig[entry].custom_sections != null
+                ) {
+                    const cssClassFields: any[] = this._getCustomCssFields(this.ccConfig[entry].custom_sections);
+    
+                    cssClassFields.forEach(
+                        (model: string) => {
+                            const configValue = this.configuration[model];
+    
+                            if (!configValue) {
+                                return;
+                            }
+    
+                            if (typeof configValue === 'string') {
+                                cssClasses.push(configValue);
+                            } else if (typeof configValue === 'object') {
+                                for (const key in configValue) {
+                                    if (configValue.hasOwnProperty(key)) {
+                                        cssClasses.push(configValue[key]);
+                                    }
                                 }
                             }
                         }
-                    }
-                );
+                    );
+                }
+            });
 
-                this.configuration.cc_css_classes = cssClasses.join(' ');
-            }
+            this.configuration.cc_css_classes = cssClasses.length ? cssClasses.join(' ') : '';
         },
         onChange(event?: Event): void {
             // Serialize reactive data.
