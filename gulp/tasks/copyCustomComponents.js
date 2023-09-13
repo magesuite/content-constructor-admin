c4ed48044305ba258bff39139cb62efc10afcce0
msiewierska <marta.siewierska-stanczak@creativestyle.pl>
2023-09-13 11:58:59 +0200

    feat: [NPW-48] Add possibility to build custom components from more than 1 repository

--- gulp/tasks/copyCustomComponents.js
+++ gulp/tasks/copyCustomComponents.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const fse = require('fs-extra');
 const glob = require('glob');
 const log = require('fancy-log');
+var path = require('path');
 
 const environment = require('../environment');
 const settings = require('../config/copyCustomComponents');
@@ -15,31 +16,68 @@ module.exports = function copyCustomComponents() {
         gulp.watch(settings.watch, copyCustomComponents);
     }
 
-    return glob(settings.sourcePath, function (err, files) {
+    return glob(settings.sourcePath, function (err, folders) {
         if (err) {
             log.error(err);
         }
 
-        if (files.length && files.length === 1) {
-            log.info('Custom content constructor components found. Copying...');
+        if (folders.length > 0) {
+            log.info('Custom content constructor components found.');
 
-            files.forEach(file => {
-                fse.copySync(file, settings.destinationPath, { overwrite: true }, function (err) {
-                    if (err) {
-                        log.error(err);
+            folders.forEach(folder => {
+                log.info(`Copying ${folder} directory`);
+
+                fse.readdirSync(folder).forEach(element => {
+                    const elementPath = path.join(folder, element);
+                    const destinationPath = path.join(settings.destinationPath, element);
+                    const isDirectory =  fse.lstatSync(elementPath).isDirectory();
+                    
+                    if (isDirectory) {
+                        fse.copySync(elementPath, destinationPath, { overwrite: true }, function (err) {
+                            if (err) {
+                                log.error(err);
+                            }
+                        });
+                    } else {
+                        if (path.extname(element) === '.scss') {
+                            if (fse.existsSync(destinationPath)) { 
+                                const existingContent = fse.readFileSync(destinationPath, {encoding: 'utf8'});
+                                const newContent = fse.readFileSync(elementPath, {encoding: 'utf8'});
+
+                                fse.writeFileSync(destinationPath, existingContent + newContent, 'utf8', {encoding: 'utf8'});
+                            } else {
+                                fse.copyFileSync(elementPath, destinationPath);
+                            }
+                        } else if (path.extname(element) === '.ts') {
+                            if (fse.existsSync(destinationPath)) { 
+                                const existingContent = fse.readFileSync(destinationPath, {encoding: 'utf8'});
+                                const newContent = fse.readFileSync(elementPath, {encoding: 'utf8'});
+
+                                let imports = newContent.split('export const customComponentsConfigurator = {')[0];
+
+                                let configuratorImports = newContent.split('export const customComponentsConfigurator = {')[1];
+                                configuratorImports = configuratorImports.split('};')[0];
+
+                                let previewImports = newContent.split('export const customComponentsPreview = {')[1];
+                                previewImports = previewImports.split('};')[0];
+
+                                let combinedContent = existingContent.split('export const customComponentsConfigurator = {');
+                                combinedContent = imports + combinedContent[0] + 'export const customComponentsConfigurator = {' + configuratorImports + combinedContent[1];
+                                combinedContent = combinedContent.split('export const customComponentsPreview = {');
+                                combinedContent = combinedContent[0] + 'export const customComponentsPreview = {' + previewImports + combinedContent[1];
+                                
+                                fse.writeFileSync(destinationPath, combinedContent, 'utf8', {encoding: 'utf8'});
+                            } else {
+                                fse.copyFileSync(elementPath, destinationPath);
+                            }
+                        } else {
+                            fse.copyFileSync(elementPath, destinationPath);
+                        }
                     }
                 });
             });
         } else {
-            if (files.length > 1) {
-                log.error('Found more content-constructor-components. Only one entry is allowed.')
-            }
-
-            if (files.length === 0) {
-                log.error('Custom content-constructor-components not found.')
-            }
-
-            log.info('Copying default entries instead...');
+            log.info('Custom content-constructor-components not found. Copying default entries instead...');
 
             fse.copySync(settings.defaultPath, settings.destinationPath, { overwrite: true }, function (err) {
                 if (err) {
