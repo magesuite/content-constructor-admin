b1aeeb366ba4da45aac71a6e9dcb322dd28dac4a
Adam Dudel <adam.dudel@creativestyle.pl>
2025-07-03 05:17:21 +0000

    feat: MGS-5397 added system clipboard functionality, moved clipboard...

---
+++ view/adminhtml/src/utils/clipboard/adapter/local-storage-clipboard.ts
@@ -0,0 +1,43 @@
+import { ClipboardAdapter, IComponentInformation } from "./types";
+
+export class LocalStorageClipboard implements ClipboardAdapter {
+    private storageKey = "magesuite-cc-admin-copied-components";
+    private maxStorageSize = 2 * 1024 * 1024; // 2MB
+
+    addComponent(component: IComponentInformation): ClipboardAdapter {
+        const components = this.getComponents();
+        components.push(component);
+
+        if (!this.validateDataSize(components)) {
+            components.shift();
+        }
+
+        return this.setComponents(components);
+    }
+
+    setComponents(components: IComponentInformation[]): ClipboardAdapter {
+        localStorage.setItem(this.storageKey, JSON.stringify(components));
+
+        return this;
+    }
+
+    getComponents(): IComponentInformation[] {
+        const components= localStorage.getItem(this.storageKey) || '[]';
+
+        return JSON.parse(components);
+    }
+
+    clear(): ClipboardAdapter {
+        localStorage.removeItem(this.storageKey);
+
+        return this;
+    }
+
+    private validateDataSize(data: IComponentInformation[]): boolean {
+        const jsonString = JSON.stringify(data);
+
+        return new TextEncoder().encode(jsonString).length <= this.maxStorageSize;
+    }
+}
+
+
