fe8d427a8eaeaee80390501faf20eb661abae8a8
Dariusz Twardowski <dariusz.twardowski@creativestyle.pl>
2026-06-19 12:08:49 +0200

    revert 'feat/MGS-5732/remove-reindex-all' into '4.x'

--- Test/Integration/Controller/Component/ProductTeaserDataTest.php
+++ Test/Integration/Controller/Component/ProductTeaserDataTest.php
@@ -58,7 +58,7 @@ class ProductTeaserDataTest extends \Magento\TestFramework\TestCase\AbstractBack
     {
         $indexerRegistry = \Magento\TestFramework\Helper\Bootstrap::getObjectManager()
             ->create(\Magento\Framework\Indexer\IndexerRegistry::class);
-        $indexerRegistry->get(\Magento\CatalogSearch\Model\Indexer\Fulltext::INDEXER_ID)->reindexRow(1);
+        $indexerRegistry->get(\Magento\CatalogSearch\Model\Indexer\Fulltext::INDEXER_ID)->reindexAll();
         $indexerRegistry->get(\Magento\Catalog\Model\Indexer\Product\Price\Processor::INDEXER_ID)->reindexRow(1);
     }
 }
